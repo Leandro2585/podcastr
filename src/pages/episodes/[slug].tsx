f134f5dea44fe5533809ece3783d6c802dc50ee9
Leandro Real <leo.real2585@gmail.com>
2021-04-21 15:34:48 -0300

    nextlevelweek#5 - trilha react - aula 3

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,86 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { format, parseISO } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import covertDurationToTimeString from '../../utils/convertDurationToTimeString'
+import api from '../../services/api'
+import styles from '../../styles/episode.module.scss'
+
+interface Episode {
+  id: string;
+  title: string;
+  description: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+
+interface EpisodeProps {
+  episode: Episode;
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+  const router = useRouter()
+  return(
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+        <button type="button">
+          <img src="/arrow-left.svg" alt="Voltar"/>
+        </button>
+        </Link>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"/>
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio"/>
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div className={styles.description} dangeourslySetInnerHTML={{ __html: episode.description }}/>
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { slug } = context.params
+  const { data } = await api.get(`/episodes/${slug}`)
+  const episode = {
+      id: data.id,
+      title: data.title,
+      thumbnail: data.thumbnail,
+      members: data.members,
+      publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+      durationAsString: covertDurationToTimeString(Number(data.file.duration)),
+      duration: Number(data.file.duration),
+      description: data.description,
+      url: data.file.url
+    }
+  return {
+    props: {
+      episode
+    },
+    revalidate: 60 * 60 * 24
+  }
+}
